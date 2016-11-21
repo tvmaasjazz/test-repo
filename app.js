7f92bef3d40ce5bc5c264f5ed69151c54b35e24b
Tyler Vander Maas <tvmaasjazz@gmail.com>
2016-11-21 10:16:33 -0800

    revert back to when had dogs file

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("This is app.js-- huge app right now");
+console.log("Hello again");
+aergfdsdffwsdwdff
--- cats.js
+++ cats.js
@@ -1,2 +1,2 @@
 console.log("Hi I'm a cat");
-console.log("now we are starting from here again");
+console.log("purr me");
